ffd864bf1c15cc654e5386d6dc4f024d49b98197
Yaw <t92t161@msu.montana.edu>
2019-03-22 11:51:15 -0600

    Network generation bug fix.

--- src/dataStore/DataStorer.java
+++ src/dataStore/DataStorer.java
@@ -99,17 +99,18 @@ public class DataStorer {
     public void generateCandidateGraph() {
         loadNetworkCosts();
         generateDelaunayPairs();
-        
+
         Object[] graphComponents = solver.generateDelaunayCandidateGraph();
         graphVertices = (int[]) graphComponents[0];
         graphEdgeCosts = (HashMap<Edge, Double>) graphComponents[1];
         graphEdgeRoutes = (HashMap<Edge, int[]>) graphComponents[2];
-        DataInOut.saveCandidateGraph();
 
         // Make right of way and construction costs
         Object[] costComponents = solver.makeComponentCosts();
         graphEdgeRightOfWayCosts = (HashMap<Edge, Double>) costComponents[0];
         graphEdgeConstructionCosts = (HashMap<Edge, Double>) costComponents[1];
+        
+        DataInOut.saveCandidateGraph();
     }
 
     public void loadNetworkCosts() {
